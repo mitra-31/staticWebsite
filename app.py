207fd9d58667a9a33cf29c9e951373d1b3b819b9
mitra-31 <pavan.pushyamitra99@gmail.com>
2021-06-02 17:52:16 +0530

    Changed app file

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/cpp")
+def cpp():
+    return render_template("cpp.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
