bca97adc761a9453a06a61c4e5e5d471b9a8cf4f
mitra-31 <pavan.pushyamitra99@gmail.com>
2021-06-03 12:20:55 +0530

    Updated Html

--- app.py
+++ app.py
@@ -14,7 +14,47 @@ def home():
 def cpp():
     return render_template("cpp.html")
 
+@app.route("/c")
+def c():
+    return render_template("develop.html")
 
+
+@app.route("/java")
+def java():
+    return render_template("develop.html")
+
+
+@app.route("/python")
+def python():
+    return render_template("develop.html")
+
+
+@app.route("/cpp-quiz")
+def cppQuiz():
+    return render_template("cpp.html")
+
+@app.route("/c-quiz")
+def cQuiz():
+    return render_template("develop.html")
+
+
+@app.route("/java-quiz")
+def javaQuiz():
+    return render_template("develop.html")
+
+
+@app.route("/python-quiz")
+def pythonQuiz():
+    return render_template("develop.html")
+
+@app.route("/python_compiler")
+def python_compiler():
+    return render_template("editor.html")
+
+
+@app.route("/java_compiler")
+def java_compiler():
+    return render_template("editor1.html")
 
 
 
